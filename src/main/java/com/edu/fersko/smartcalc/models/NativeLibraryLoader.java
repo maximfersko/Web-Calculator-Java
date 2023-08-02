6101ac483794ef8f5135386d7aafb6992e7d4333
maximfersko <79246330+maximfersko@users.noreply.github.com>
2023-08-03 00:45:41 +0700

    refactoring

--- src/main/java/com/edu/fersko/smartcalc/models/NativeLibraryLoader.java
+++ src/main/java/com/edu/fersko/smartcalc/models/NativeLibraryLoader.java
@@ -5,9 +5,9 @@ import java.nio.file.Paths;
 
 public class NativeLibraryLoader {
     public static String getLibraryPath(boolean isMain) {
-        String libraryRelativePath = isMain
-                ? "src/main/java/com/edu/fersko/smartcalc/models/lib/SmartCore.dll"
-                : "src/main/java/com/edu/fersko/smartcalc/models/lib/CreditCore.dll";
+        String libraryName = isMain ? "SmartCore" : "CreditCore";
+        String libraryExtension = System.getProperty("os.name").toLowerCase().contains("win") ? ".dll" : ".so";
+        String libraryRelativePath = "src/main/java/com/edu/fersko/smartcalc/models/lib/" + libraryName + libraryExtension;
 
         Path absolutePath = Paths.get("").toAbsolutePath();
         return absolutePath.resolve(libraryRelativePath).toString();
