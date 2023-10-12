981bdc3782825d97264d5ce819d3319b070d7c50
maximfersko <79246330+maximfersko@users.noreply.github.com>
2023-10-13 00:27:43 +0700

    release version

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function createWindow() {
         },
     });
 
-mainWindow.loadURL('http://localhost:3003');
+mainWindow.loadURL('http://localhost:8888');
 
     mainWindow.on('closed', () => {
         mainWindow = null;
